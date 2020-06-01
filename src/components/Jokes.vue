81ff0f43b0fceb8b188a4da455f6def3a4a0fd28
Daniel Subasic <subasic97@gmail.com>
2020-06-01 16:16:12 +0200

    La till meddelande som visas när listan med skämt är tom

--- src/components/Jokes.vue
+++ src/components/Jokes.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="jokes_container">
+      <md-empty-state v-if="jokes.length ==0" md-icon="error" md-label="No jokes added" md-description="Jokes that you mark as favorite will be listed here."></md-empty-state>
       <Joke :style="'animation-delay: ' + (0.05 * i).toString() + 's' " class="list-item" v-for="(jokeid, i) in jokes" :key="jokeid" v-bind:jokeId="jokeid"/>
   </div>
 </template>
